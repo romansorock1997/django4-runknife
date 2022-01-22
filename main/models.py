834f1992d0d9b516249aa80415927762a18c6ae3
Qdysky <blashirebel@mail.com>
2022-01-22 05:12:46 +0300

    addition migrations

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
+from email.mime import image
+from turtle import title
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100) # длина заголовка
+    description = models.CharField(max_length=250)
+    image = models.ImageField()
+    url = models.URLField(blank=True)
